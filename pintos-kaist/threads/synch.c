dc2609c15339770af33f3d44e74ccb2fa909fb9a
ChangHyun Park <buschanny@gmail.com>
2025-05-13 22:06:05 +0900

    Add : pass priority-sema

--- pintos-kaist/threads/synch.c
+++ pintos-kaist/threads/synch.c
@@ -66,7 +66,7 @@ sema_down (struct semaphore *sema) {
 
 	old_level = intr_disable ();
 	while (sema->value == 0) {
-		list_push_back (&sema->waiters, &thread_current ()->elem);
+		list_insert_ordered(&sema->waiters, &thread_current ()->elem, priority_more, NULL);
 		thread_block ();
 	}
 	sema->value--;
@@ -109,10 +109,17 @@ sema_up (struct semaphore *sema) {
 	ASSERT (sema != NULL);
 
 	old_level = intr_disable ();
-	if (!list_empty (&sema->waiters))
-		thread_unblock (list_entry (list_pop_front (&sema->waiters),
-					struct thread, elem));
 	sema->value++;
+
+	if (!list_empty (&sema->waiters)){
+
+		struct thread *curr = list_entry(list_pop_front(&sema->waiters), struct thread, elem);
+		thread_unblock(curr);
+		if(thread_current()->priority < curr->priority){
+			thread_yield();
+		}
+	}
+		
 	intr_set_level (old_level);
 }
 
